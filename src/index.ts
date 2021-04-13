cd9b8a4b1b154322f9f8f06b19e4ac6c5b60af18
Brian Faust <hello@basecode.sh>
2021-04-13 13:43:50 +0300

    chore: integrate `@konceiver/tease`

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
+/* eslint-disable jest/no-export */
+
 import { ILogger } from "@konceiver/monolog";
 
 export const complianceTests = (logger: ILogger) => {
-	test.each(["emergency", "alert", "critical", "error", "warning", "notice", "info", "debug"])(
-		".%s(message)",
-		level => {
-			const spyLog: jest.SpyInstance = jest.spyOn(logger, "log");
+	test.each([
+		"emergency",
+		"alert",
+		"critical",
+		"error",
+		"warning",
+		"notice",
+		"info",
+		"debug",
+	])(".%s(message)", (level) => {
+		const spyLog: jest.SpyInstance = jest.spyOn(logger, "log");
 
-			// @ts-ignore
-			logger[level]("Hello World");
+		// @ts-ignore
+		logger[level]("Hello World");
 
-			expect(spyLog).toHaveBeenCalledWith(level, "Hello World", undefined);
+		expect(spyLog).toHaveBeenCalledWith(level, "Hello World", undefined);
 
-			spyLog.mockReset();
-		},
-	);
+		spyLog.mockReset();
+	});
 };
